bf8aa3c7d0e254d7d9d15e335864e68cfc1d925e
Efox <efox.web@gmail.com>
2022-04-13 22:12:47 -0300

    Update defaults.js

--- www/nodejs-project/modules/config/defaults.js
+++ www/nodejs-project/modules/config/defaults.js
@@ -53,7 +53,7 @@ module.exports = {
 	'parental-control-policy': 'block',
 	'parental-control-terms': '.',
 	'playback-rate-control': true,
-	'prefer-ipv6': true,
+	'prefer-ipv6': false,
 	'connect-timeout': 5,
 	'resume': false,
 	'search-missing-logos': true,
