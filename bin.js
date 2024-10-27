a9d4b78220c54149d753b62b95c25ddcdc52a39b
Edenware <efox.web@gmail.com>
2024-10-27 15:35:41 -0300

    v17.5.4
    - Resolve compatibility issues with 'npx megacubo'.

--- bin.js
+++ bin.js
@@ -5,11 +5,12 @@ import fs from "fs";
 import { getDirname} from "cross-dirname";
 
 const __dirname = getDirname();
+const debug = process.argv.includes('debug') || process.argv.includes('--inspect');
 
 async function findElectronExecutable() {
     const relativePaths = [
         'node_modules/electron/dist/electron',
-        'www/nodejs/node_modules/electron/dist/electron'
+        '../electron/dist/electron'
     ];
     for (const relativePath of relativePaths) {
         const fullPath = path.resolve(__dirname, relativePath);
@@ -17,8 +18,7 @@ async function findElectronExecutable() {
         try {
             await fs.promises.access(executable, fs.constants.F_OK);
             return executable;
-        }
-        catch (error) {}
+        } catch (error) { }
     }
     // Check environment variable
     const environmentPath = process.env.ELECTRON_PATH;
@@ -27,9 +27,7 @@ async function findElectronExecutable() {
             await fs.promises.access(environmentPath, fs.constants.F_OK);
             return environmentPath;
         }
-        catch (error) {
-            // File not found
-        }
+        catch (error) { }
     }
     // Check global NPM installation directory
     const npmGlobalPrefix = process.env.npm_global_prefix;
@@ -40,20 +38,47 @@ async function findElectronExecutable() {
             await fs.promises.access(globalExecutableWithExtension, fs.constants.F_OK);
             return globalExecutableWithExtension;
         }
-        catch (error) {
-            // File not found
-        }
+        catch (error) { }
     }
     // Default return if not found
     return null;
 }
 findElectronExecutable().then(electronPath => {
     if (electronPath) {
-        const child = spawn(electronPath, [path.join(__dirname, 'www/nodejs/dist/main.js')], {
+        const params = []
+        if(debug) {
+            params.push(...[
+                '--inspect',
+                '--enable-logging=stderr',
+                '--trace-warnings',
+                '--remote-debugging-port=9222'
+            ])
+        }
+        params.push(path.join(__dirname, 'www/nodejs/dist/main.js'));
+        const opts = debug ? {} : {
             detached: true,
             stdio: 'ignore',
-        });
-        child.unref();
+        };
+        const child = spawn(electronPath, params, opts);
+        console.log({electronPath, params, opts})
+        if(debug) {                
+            child.stdout.on('data', (data) => {
+                process.stdout.write(data);
+            });
+            child.stderr.on('data', (data) => {
+                process.stderr.write(data);
+            });
+            child.on('error', (error) => {
+                console.error(error);
+            });
+            child.once('close', (code) => {
+                console.log('exitcode: ' + code);
+                process.exit(code);
+            });
+            return;
+        } else {
+            child.unref();
+        }
     } else {
         console.error('Electron executable not found. Use \'npm i electron@9.1.2\' to install it.');
     }
