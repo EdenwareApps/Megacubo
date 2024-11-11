919a88e6c8faba8f6161c7f492436ed3a10ebf87
Edenware <efox.web@gmail.com>
2024-11-11 15:57:14 -0300

    v17.5.5

--- www/nodejs/modules/jimp-worker/main.js
+++
@@ -1,5 +0,0 @@
-import path from 'path'
-import workers from '../multi-worker/main.js'
-import { getDirname } from 'cross-dirname'
-
-export default workers.load(path.join(getDirname(), 'jimp-worker.js'))
